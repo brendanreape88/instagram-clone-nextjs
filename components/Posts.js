07467733b6559bb83411d0c7b61c40fb5e9bd486
Brendan Reape <brendanreape88@gmail.com>
2021-12-21 21:18:49 +0000

    pulling in posts from firebase

--- components/Posts.js
+++ components/Posts.js
@@ -1,26 +1,34 @@
 import Post from "./Post";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+import { useEffect, useState } from "react";
 
 function Posts() {
-  const posts = [
-    {
-      id: 1,
-      username: "Bill",
-      userImg: "https://i.ibb.co/TMm6BWS/Bill.png",
-      img: "https://i.ibb.co/yyX2rLs/Post-1.png",
-      caption: "Check out this cool app I built!",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const unsubcribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+
+    return () => {
+      unsubcribe();
+    };
+  }, [db]);
 
   return (
     <div>
-      {posts?.map((post) => (
+      {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
