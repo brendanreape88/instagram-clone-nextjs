7e6699278a36bc64c085e88af3854975ac92c64b
Brendan Reape <brendanreape88@gmail.com>
2021-12-21 21:16:03 +0000

    uplod to firebase storage active

--- components/Modal.js
+++ components/Modal.js
@@ -3,7 +3,16 @@ import { CameraIcon } from "@heroicons/react/outline";
 import { modalState } from "../atoms/modalAtom";
 import { useRecoilState } from "recoil";
 import { Fragment, useRef, useState } from "react";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+import { db, storage } from "../firebase";
 import { useSession } from "next-auth/react";
+import { ref, getDownloadURL, uploadString } from "@firebase/storage";
 
 function Modal() {
   const { data: session } = useSession();
@@ -12,6 +21,35 @@ function Modal() {
   const [selectedFile, setSelectedFile] = useState(null);
   const captionRef = useRef(null);
   const filePickerRef = useRef(null);
+
+  const uploadPost = async () => {
+    if (loading) return;
+
+    setLoading(true);
+    const docRef = await addDoc(collection(db, "posts"), {
+      username: session.user.username,
+      caption: captionRef.current.value,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+
+    console.log("New doc added with ID", docRef.id);
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+    await uploadString(imageRef, selectedFile, "data_url").then(
+      async (snapshot) => {
+        const downloadURL = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id), {
+          image: downloadURL,
+        });
+      }
+    );
+
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+  };
 
   const addImageToPost = (e) => {
     const reader = new FileReader();
@@ -112,7 +150,7 @@ function Modal() {
                   type="button"
                   disabled={!selectedFile}
                   className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300"
-                  //   onClick={uploadPost}
+                  onClick={uploadPost}
                 >
                   {loading ? "Uploading..." : "Upload Post"}
                 </button>
