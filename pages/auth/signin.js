b0d8d8cb641d7a40c765c40ded8bc1c55f7a1ef6
Brendan Reape <brendanreape88@gmail.com>
2021-12-21 19:16:24 +0000

    google sign in added

---
+++ pages/auth/signin.js
@@ -0,0 +1,43 @@
+import { getProviders, signIn as signIntoProvider } from "next-auth/react";
+
+export default function SignIn({ providers }) {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+        <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
+        <p className="font-xs italic">
+          This app has been built for demonstration purposes only.
+        </p>
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() => signIntoProvider(provider.id)}
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+// This is the recommended way for Next.js 9.3 or newer
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
+
+/*
+// If older than Next.js 9.3
+SignIn.getInitialProps = async () => {
+  return {
+    providers: await getProviders()
+  }
+}
+*/
